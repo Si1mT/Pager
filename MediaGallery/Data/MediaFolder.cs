9066c86ca051b6df5cfef1d9ca0643d4054e7067
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-12-18 11:46:47 +0200

    initial commit

---
+++ MediaGallery/Data/MediaFolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace MediaGallery.Data
+{
+    public class MediaFolder : MediaItem 
+    {
+        public PagedResult<MediaItem> PageResult { get; set; }
+
+        public IList<MediaItem> Items { get; set; }
+        public override string Thumbnail
+        {
+            get
+            {
+                return "folder.jpg";
+            }
+            set { }
+        }
+
+        public MediaFolder()
+        {
+            Items = new List<MediaItem>();
+        }
+    }
+}
---
+++ MediaGallery/Models/FolderModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaGallery.Data;
+
+namespace MediaGallery.Models
+{
+    public class FolderModel : MediaItem
+    {
+        public PagedResult<MediaItem> PageThing { get; set; }
+
+        public IList<MediaItem> Items { get; set; }
+        public override string Thumbnail
+        {
+            get
+            {
+                return "folder.jpg";
+            }
+            set { }
+        }
+
+        public FolderModel()
+        {
+            Items = new List<MediaItem>();
+        }
+    }
+}
